a351136cef37da061b824560aa6688ba852701ac
AgnesGeller <agnesgeller@cyberanalys.com>
2026-08-17 17:43:18 +0200

    Use distinctive M worksheet app icon

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE="diszkertek-onallo-munkalap-v11";
-const OFFLINE_PAGE="./index.html?v=11";
-const ASSETS=[OFFLINE_PAGE,"./styles.css","./logo-overrides.css","./app.js?v=10","./manifest.webmanifest?v=11","./official-logo.png","./official-emblem.png","./botanical.svg","./official-icon-192.png","./diszkertek-logo-32-v11.png","./diszkertek-logo-192-v11.png","./diszkertek-logo-512-v11.png","./diszkertek-logo-maskable-512-v11.png","./diszkertek-logo-apple-180-v11.png","./favicon-v11.ico"];
+const CACHE="diszkertek-onallo-munkalap-v12";
+const OFFLINE_PAGE="./index.html?v=12";
+const ASSETS=[OFFLINE_PAGE,"./styles.css","./logo-overrides.css","./app.js?v=10","./manifest.webmanifest?v=12","./official-logo.png","./official-emblem.png","./botanical.svg","./official-icon-192.png","./munkalap-m-icon-32-v12.png","./munkalap-m-icon-192-v12.png","./munkalap-m-icon-512-v12.png","./munkalap-m-icon-maskable-512-v12.png","./munkalap-m-icon-apple-180-v12.png","./munkalap-m-favicon-v12.ico"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
