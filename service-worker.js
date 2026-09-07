1b96988050832f0d04196d5fad4cc4dee4291fa9
AgnesGeller <agnesgeller@cyberanalys.com>
2026-09-07 09:54:56 +0200

    Remove unnecessary interface explanations

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE="diszkertek-onallo-munkalap-v36";
-const OFFLINE_PAGE="./index.html?v=36";
-const ASSETS=[OFFLINE_PAGE,"./styles.css?v=36","./logo-overrides.css?v=36","./app-shell.css?v=36","./app.js?v=36","./billing-math.js?v=36","./billing-settlements.js?v=36","./statistics-settlements.js?v=36","./supabase.js?v=36","./supabase-config.js?v=36","./munkalap-data.js?v=36","./manifest.webmanifest?v=36","./official-logo.png","./official-emblem.png","./botanical.svg","./official-icon-192.png","./munkalap-m-icon-32-v12.png","./munkalap-m-icon-192-v12.png","./munkalap-m-icon-512-v12.png","./munkalap-m-icon-maskable-512-v12.png","./munkalap-m-icon-apple-180-v12.png","./munkalap-m-favicon-v12.ico"];
+const CACHE="diszkertek-onallo-munkalap-v37";
+const OFFLINE_PAGE="./index.html?v=37";
+const ASSETS=[OFFLINE_PAGE,"./styles.css?v=37","./logo-overrides.css?v=37","./app-shell.css?v=37","./app.js?v=37","./billing-math.js?v=37","./billing-settlements.js?v=37","./statistics-settlements.js?v=37","./supabase.js?v=37","./supabase-config.js?v=37","./munkalap-data.js?v=37","./manifest.webmanifest?v=37","./official-logo.png","./official-emblem.png","./botanical.svg","./official-icon-192.png","./munkalap-m-icon-32-v12.png","./munkalap-m-icon-192-v12.png","./munkalap-m-icon-512-v12.png","./munkalap-m-icon-maskable-512-v12.png","./munkalap-m-icon-apple-180-v12.png","./munkalap-m-favicon-v12.ico"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
